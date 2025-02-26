462b0edf3876e273e604aee97e6aa48d68968fb2
Lucian Hanga <lucianhanga@gmx.net>
2025-02-26 19:29:40 +0100

    Add ImageAvailable function to process ID documents from blob storage and store results in Azure Table Storage

--- buletine-api/function_app.py
+++ buletine-api/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 from UploadImage import upImg
-from BlobTriggerFunction import blTrig
+from ImageAvailable import blTrig
 from PullResults import pullRes
 app = func.FunctionApp()
 app.register_blueprint(upImg)
